c11a6e9f7184f9e5de67c4821da70bba11d9619c
ShixingWang <wangshixing@wustl.edu>
2019-07-30 08:35:05 -0500

    minor modify

---
+++ lesion/shixing.py
@@ -0,0 +1,20 @@
+import lifio
+import glob
+
+folderStacked = "D:/Documents/Files/imagesSYNC/MLtraining/AllReady/StackCrop/"
+filderSingles = "D:/Documents/Files/imagesSYNC/MLtraining/AllReady/SHIXING4/"
+
+channelsOrganelles = [
+    "vacuole",
+    "ER",
+    "golgi",
+    "lipid",
+    "mitochondria",
+]
+channelsCamera = [
+    "DAPI",
+    "CFP",
+    "FITC",
+    "YFP",
+    "TRITC"
+]
